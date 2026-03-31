206037c1d9d50c3585676cc3c3006888ee612a5f
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-03-31 08:41:33 +0500

    Adjusted page

---
+++ src/data/sections.ts
@@ -0,0 +1,19 @@
+export interface Section {
+  title: string;
+  desc: string;
+  path: string;
+  icon: string;
+}
+
+export const sections: Section[] = [
+  { title: 'Какие налоги оплачивать', desc: 'Таблица платежей, суммы, формулы, графики оплаты', path: '/taxes/', icon: '💸' },
+  { title: 'FAQ по валютному контролю', desc: 'Вопросы по валютному контролю для ИП', path: '/faq-currency/', icon: '👮' },
+  { title: 'FAQ и прочие вопросы', desc: 'Форма 910, ОКЭД, форма 270, TIN, БИК, крипта', path: '/faq/', icon: '❓' },
+  { title: 'Как зарегистрироваться на упрощенку', desc: 'Пошаговая инструкция со скриншотами на 2026 год', path: '/registration/', icon: '🎭' },
+  { title: 'Ставка налога по регионам', desc: 'Ставки подоходного налога по городам Казахстана', path: '/tax-rates/', icon: '📉' },
+  { title: 'Пример инвойса', desc: 'Шаблон инвойса для выписывания компании', path: '/invoice/', icon: '📝' },
+  { title: 'Как получить УНК', desc: 'Учетный номер контракта для валютного контроля', path: '/unk/', icon: '📑' },
+  { title: 'Банки для ИП', desc: 'Что выбрать и как работать с банками', path: '/banks/', icon: '🏦' },
+  { title: 'ИП в Грузии', desc: 'Сравнение с Грузией (TLDR: не советуем)', path: '/georgia/', icon: '🚈' },
+  { title: 'Полезные ссылки', desc: 'Полезные ссылки для разработчиков', path: '/links/', icon: '🔗' },
+];
